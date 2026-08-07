861364751c36c1f9f950c361609116fb901563b0
Jeroen Meijer (Jay) <jeroenfkmeijer@gmail.com>
2026-08-07 11:43:10 +0200

    chore: release 1.5.1 (#19)

--- lib/src/misc/constants.dart
+++ lib/src/misc/constants.dart
@@ -6,7 +6,7 @@ class Constants {
   static const appName = 'InPhase';
   static const packageName = 'in_phase';
   static const commandName = 'in_phase';
-  static const version = '1.5.0';
+  static const version = '1.5.1';
 
   static final appDataDir = Directory(path.join(_getUserDir(), '.in_phase'));
 
